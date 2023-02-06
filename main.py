6e68a24a2420423358990775394bd577b528e1df
sixicode <109619614+sixicode@users.noreply.github.com>
2023-02-07 00:58:53 +0900

    debugging

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def paper(doi1, doi2, page):
     try:
         doi=doi1+'/'+doi2
         pv=paper_viewer(doi)
+        print('debug-1')
         pv.download_pdf()
+        print('debug-2')
         pv.pdf2image()
+        print('debug-3')
         pv.image2mp4()
+        print('debug-4')
         return send_file(f'./result_{pv.file_name}/{pv.file_name}{page}.mp4', f'{pv.file_name}{page}.mp4', as_attachment=True)
     except:
         return 'something went wrong'
