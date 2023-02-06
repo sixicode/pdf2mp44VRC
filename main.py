3165cd7df3c28457a9c6e6c415d431de7dbda641
sixicode <109619614+sixicode@users.noreply.github.com>
2023-02-07 00:23:53 +0900

    goooood

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'Hello, World!'
+    return 'There is nothing here...'
 
 if __name__ == '__main__':
     app.run(debug=True)
