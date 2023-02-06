df6927bbda85defbd41413f0d40fac65e257c457
sixicode <109619614+sixicode@users.noreply.github.com>
2023-02-07 00:55:57 +0900

    같은 실수 또 하네

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import paper_viewer
 app = Flask(__name__)
 
 @app.route('/paper/<doi1>/<doi2>/<page>')
-def paper(doi1, doi2):
+def paper(doi1, doi2, page):
     try:
         doi=doi1+'/'+doi2
         pv=paper_viewer(doi)
