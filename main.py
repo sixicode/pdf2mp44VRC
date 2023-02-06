c65449a4f5c9b90586007b5524b80b541c1028c4
sixicode <109619614+sixicode@users.noreply.github.com>
2023-02-07 02:01:38 +0900

    아 싯팔 주석

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def paper(doi1, doi2, page):
         doi=f'{doi1}/{doi2}'
         pv=paper_viewer.paper_viewer(doi)
         pv.download_pdf()
-        # pv.pdf2image()
-        # pv.image2mp4()
-        # return send_file(f'./result_{pv.file_name}/{pv.file_name}{page}.mp4', f'{pv.file_name}{page}.mp4', as_attachment=True)
+        pv.pdf2image()
+        pv.image2mp4()
+        return send_file(f'./result_{pv.file_name}/{pv.file_name}{page}.mp4', f'{pv.file_name}{page}.mp4', as_attachment=True)
     except:
         return 'something went wrong'
 
