c18823019d54c520ee792c7a3818f606954a5a38
sixicode <109619614+sixicode@users.noreply.github.com>
2023-02-07 01:52:21 +0900

    duplicate check

--- paper_viewer.py
+++ paper_viewer.py
@@ -5,6 +5,7 @@ import cv2
 class paper_viewer:
     def __init__(self, doi):
         self.doi=doi
+        self.file_name=((self.doi).split('/')[1]).replace('/', '.', 2)
 
     # def get_available_url(self):
     #     res=os.popen('scihub -c')
@@ -13,26 +14,33 @@ class paper_viewer:
 
     def download_pdf(self):
         try:
+            file_list=os.popen('ls pdf')
+            if f'{self.file_name}.pdf' in file_list:
+                return
             # print(f'scihub -s {self.doi}')
             os.system(f'scihub -s {self.doi}')
         except:
             print('download error')
     
     def pdf2image(self):
-        self.file_name=((self.doi).split('/')[1]).replace('/', '.', 2)
         pages = convert_from_path(f"pdf/{self.file_name}.pdf")
         self.num_page=len(pages)
         for i, page in enumerate(pages):
 	        page.save(f"./pdf/{self.file_name}{str(i)}.jpg", "JPEG")
         
     def image2mp4(self):
+        if result_{self.file_name} in os.popen('ls'):
+            pass
+        else
+            os.system(f'mkdir result_{self.file_name}')
+
         # encoder(for mp4)
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
         # output file name, encoder, fps, size(fit to image size)
         
-        os.system(f'mkdir result_{self.file_name}')
-
         for i in range(self.num_page):
+            if f'result_{self.file_name}/{self.file_name}{i}.mp4' in os.popen(f'ls result_{self.file_name}'):
+                continue
             img = cv2.imread(f'./pdf/{self.file_name}{i}.jpg')
             height, width, layers = img.shape
             size = (width, height)
